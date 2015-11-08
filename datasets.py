653cfcfb605b1f3a0717b1abc5be015716a63991
Ivan Vendrov <ivendrov@gmail.com>
2015-11-08 12:26:58 -0500

    added special handling of COCO dataset

--- datasets.py
+++ datasets.py
@@ -29,12 +29,18 @@ def load_dataset(name, cnn, captions, load_train=True):
     for split in splits:
         dataset[split] = {}
         caps = []
-        with open('%s/captions/%s/%s.txt' % (loc, captions, split), 'rb') as f:
+        splitName = 'val' if name == 'coco' and split == 'dev' else split
+        with open('%s/captions/%s/%s.txt' % (loc, captions, splitName), 'rb') as f:
             for line in f:
                 caps.append(line.strip())
             dataset[split]['caps'] = caps
 
-        dataset[split]['ims'] = numpy.load('%s/images/%s/%s.npy' % (loc, cnn, split))
+        dataset[split]['ims'] = numpy.load('%s/images/%s/%s.npy' % (loc, cnn, splitName))
+
+        # handle coco specially by only taking the first 1k images / 5k captions
+        if name == 'coco' and split in ['dev', 'test']:
+            dataset[split]['ims'] = dataset[split]['ims'][:1000]
+            dataset[split]['caps'] = dataset[split]['caps'][:5000]
 
     return dataset
 
